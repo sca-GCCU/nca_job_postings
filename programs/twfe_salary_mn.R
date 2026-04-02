fbdff8649d95669554288b3a06f05ccb5a10acdd
sca-GCCU <scanastasi18@gmail.com>
2026-04-02 16:39:33 -0400

    Added TWFE stuff that is conditional on experience

--- programs/twfe_salary_mn.R
+++ programs/twfe_salary_mn.R
@@ -880,6 +880,572 @@ ggsave(
 # --- CONDITIONING ON EXPERIENCE REQUIREMENTS ----------------------------------
 # ------------------------------------------------------------------------------
 
+# NOTE: Start with just real salary and then expand if needed. 
+
+# REAL SALARY ------------------------------------------------------------------
+# --- EXPERIENCE REQUIRED ------------------------------------------------------
+
+# --- 1. Filter the data --- 
+# Full TWFE dataframe 
+salary_mn_analysis_exp <- salary_mn_analysis %>%
+  filter(any_exp == 1)
+
+# Event Study dataframe 
+es_df_exp <- salary_mn_analysis %>%
+  filter(any_exp == 1) %>%
+  filter(event_time >= et_lb)
+
+
+
+# --- 2. Table ---  
+
+# (i) Spec 1: TWFE 
+twfe_real_salary_exp_1 <- feols(
+  real_salary ~ treated_eff_full | state + date,
+  data = salary_mn_analysis_exp,
+  cluster = ~state
+)
+
+summary(twfe_real_salary_exp_1)
+
+
+# (ii) Spec 2: Add Occupation FE
+twfe_real_salary_exp_2 <- feols(
+  real_salary ~ treated_eff_full | state + date + soc_4,
+  data = salary_mn_analysis_exp,
+  cluster = ~state
+)
+
+summary(twfe_real_salary_exp_2)
+
+
+# ADDING NEW SPECIFICATION 3; IGNORING CONTROL VARIABLES FOR NOW
+
+# (iii) Spec 3: Add Firm FE
+
+twfe_real_salary_exp_3 <- feols(
+  real_salary ~ treated_eff_full | state + date + soc_4 + company,
+  data = salary_mn_analysis_exp,
+  cluster = ~state
+)
+
+summary(twfe_real_salary_exp_3)
+
+
+# Combine into table 
+etable(
+  twfe_real_salary_exp_1, twfe_real_salary_exp_2, twfe_real_salary_exp_3, 
+  tex = TRUE,
+  tpt = TRUE,
+  dict = c(
+    real_salary = "Salary (Real) - Experience Requirement",
+    treated_eff_full = "Treated $\\times$ Post",
+    state = "State",
+    date = "Month-Year",
+    soc_4 = "Occupation",
+    company = "Firm"
+  ),
+  style.tex = style.tex("aer"), # Removes weird "Model" thing and cleans up headings
+  notes = c(
+    "@\\footnotesize",
+    "*Notes:* Standard errors are clustered at the state level.",
+    "*** p$<$0.01, ** p$<$0.05, * p$<$0.1"
+  ),
+  file = "output/tables/table_twfe_real_salary_exp_salary_mn.tex"
+)
+
+# NOTE: Once I'm performing other methods too, I may need to figure out how to 
+# combine all of these results in a different table. This is fine for now.
+
+
+
+
+# --- 3. Event Study ---
+
+# (i) Spec 1: TWFE 
+# Run event study specification 
+es_real_salary_exp_1 <- feols(
+  real_salary ~ i(event_time, ban_full, -1) |
+    state + date,
+  data = es_df_exp,
+  cluster = ~state
+)
+
+summary(es_real_salary_exp_1)
+
+# create plotting dataframe 
+es_real_salary_exp_1_df <- tibble(
+  term = names(es_real_salary_exp_1$coefficients),
+  estimate = as.numeric(es_real_salary_exp_1$coefficients),
+  std_error = as.numeric(es_real_salary_exp_1$se)
+) %>%
+  mutate(
+    event_time = as.numeric(str_extract(term, "-?\\d+")),
+    ci_low = estimate - 1.96 * std_error, # 95% CIs 
+    ci_high = estimate + 1.96 * std_error
+  )
+
+ref_row <- tibble( #adding back in an entry for the omitted period 
+  term = "Reference",
+  estimate = 0, 
+  std_error = NA_real_,
+  event_time = -1,
+  ci_low = NA_real_,
+  ci_high = NA_real_
+)
+
+es_real_salary_exp_1_df <- bind_rows(es_real_salary_exp_1_df, ref_row) %>%
+  arrange(event_time)
+
+# Create plot with ggplot 
+es_plot_real_salary_exp_1 <- ggplot(es_real_salary_exp_1_df, aes(x = event_time, y = estimate)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
+  geom_vline(xintercept = -1, linetype = "dotted", color = "black") +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.4) + 
+  scale_x_continuous(breaks = seq(-36, 19, by = 3)) + 
+  labs(
+    x = "Months Since Ban",
+    y = "Coefficient - Salary (Real) - Experience"
+  ) + 
+  theme_minimal() + 
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.ticks = element_line(color = "black")
+  )
+
+es_plot_real_salary_exp_1
+
+# Save the event study plot 
+ggsave(
+  "output/figures/es_real_salary_exp_1_salary_mn.pdf",
+  es_plot_real_salary_exp_1,
+  width = 7,
+  height = 4.5,
+  units = "in"
+)
+
+
+# (ii) Spec 2: Add Occupation FE
+# Run event study specification 
+es_real_salary_exp_2 <- feols(
+  real_salary ~ i(event_time, ban_full, -1) |
+    state + date + soc_4,
+  data = es_df_exp,
+  cluster = ~state
+)
+
+summary(es_real_salary_exp_2)
+
+# create plotting dataframe 
+es_real_salary_exp_2_df <- tibble(
+  term = names(es_real_salary_exp_2$coefficients),
+  estimate = as.numeric(es_real_salary_exp_2$coefficients),
+  std_error = as.numeric(es_real_salary_exp_2$se)
+) %>%
+  mutate(
+    event_time = as.numeric(str_extract(term, "-?\\d+")),
+    ci_low = estimate - 1.96 * std_error, # 95% CIs 
+    ci_high = estimate + 1.96 * std_error
+  )
+
+ref_row <- tibble( #adding back in an entry for the omitted period 
+  term = "Reference",
+  estimate = 0, 
+  std_error = NA_real_,
+  event_time = -1,
+  ci_low = NA_real_,
+  ci_high = NA_real_
+)
+
+es_real_salary_exp_2_df <- bind_rows(es_real_salary_exp_2_df, ref_row) %>%
+  arrange(event_time)
+
+# Create plot with ggplot 
+es_plot_real_salary_exp_2 <- ggplot(es_real_salary_exp_2_df, aes(x = event_time, y = estimate)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
+  geom_vline(xintercept = -1, linetype = "dotted", color = "black") +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.4) + 
+  scale_x_continuous(breaks = seq(-36, 19, by = 3)) + 
+  labs(
+    x = "Months Since Ban",
+    y = "Coefficient - Salary (Real) - Experience"
+  ) + 
+  theme_minimal() + 
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.ticks = element_line(color = "black")
+  )
+
+es_plot_real_salary_exp_2
+
+# Save the event study plot 
+ggsave(
+  "output/figures/es_real_salary_exp_2_salary_mn.pdf",
+  es_plot_real_salary_exp_2,
+  width = 7,
+  height = 4.5,
+  units = "in"
+)
+
+
+# (iii) Spec 3: Add Firm FE 
+# Run event study specification 
+es_real_salary_exp_3 <- feols(
+  real_salary ~ i(event_time, ban_full, -1) |
+    state + date + soc_4 + company,
+  data = es_df_exp,
+  cluster = ~state
+)
+
+summary(es_real_salary_exp_3)
+
+# create plotting dataframe 
+es_real_salary_exp_3_df <- tibble(
+  term = names(es_real_salary_exp_3$coefficients),
+  estimate = as.numeric(es_real_salary_exp_3$coefficients),
+  std_error = as.numeric(es_real_salary_exp_3$se)
+) %>%
+  mutate(
+    event_time = as.numeric(str_extract(term, "-?\\d+")),
+    ci_low = estimate - 1.96 * std_error, # 95% CIs 
+    ci_high = estimate + 1.96 * std_error
+  )
+
+ref_row <- tibble( #adding back in an entry for the omitted period 
+  term = "Reference",
+  estimate = 0, 
+  std_error = NA_real_,
+  event_time = -1,
+  ci_low = NA_real_,
+  ci_high = NA_real_
+)
+
+es_real_salary_exp_3_df <- bind_rows(es_real_salary_exp_3_df, ref_row) %>%
+  arrange(event_time)
+
+# Create plot with ggplot 
+es_plot_real_salary_exp_3 <- ggplot(es_real_salary_exp_3_df, aes(x = event_time, y = estimate)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
+  geom_vline(xintercept = -1, linetype = "dotted", color = "black") +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.4) + 
+  scale_x_continuous(breaks = seq(-36, 19, by = 3)) + 
+  labs(
+    x = "Months Since Ban",
+    y = "Coefficient - Salary (Real) - Experience"
+  ) + 
+  theme_minimal() + 
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.ticks = element_line(color = "black")
+  )
+
+es_plot_real_salary_exp_3
+
+# Save the event study plot 
+ggsave(
+  "output/figures/es_real_salary_exp_3_salary_mn.pdf",
+  es_plot_real_salary_exp_3,
+  width = 7,
+  height = 4.5,
+  units = "in"
+)
+
+
+
+
+
+
+# --- NO EXPERIENCE REQUIRED ---------------------------------------------------
+
+# --- 1. Filter the data ---  
+# Full TWFE dataframe 
+salary_mn_analysis_no_exp <- salary_mn_analysis %>%
+  filter(any_exp == 0)
+
+# Event Study dataframe 
+es_df_no_exp <- salary_mn_analysis %>%
+  filter(any_exp == 0) %>%
+  filter(event_time >= et_lb)
+
+
+# --- 3. Table ---  
+
+# (i) Spec 1: TWFE 
+twfe_real_salary_no_exp_1 <- feols(
+  real_salary ~ treated_eff_full | state + date,
+  data = salary_mn_analysis_no_exp,
+  cluster = ~state
+)
+
+summary(twfe_real_salary_no_exp_1)
+
+
+# (ii) Spec 2: Add Occupation FE
+twfe_real_salary_no_exp_2 <- feols(
+  real_salary ~ treated_eff_full | state + date + soc_4,
+  data = salary_mn_analysis_no_exp,
+  cluster = ~state
+)
+
+summary(twfe_real_salary_no_exp_2)
+
+
+# ADDING NEW SPECIFICATION 3; IGNORING CONTROL VARIABLES FOR NOW
+
+# (iii) Spec 3: Add Firm FE
+
+twfe_real_salary_no_exp_3 <- feols(
+  real_salary ~ treated_eff_full | state + date + soc_4 + company,
+  data = salary_mn_analysis_no_exp,
+  cluster = ~state
+)
+
+summary(twfe_real_salary_no_exp_3)
+
+
+# Combine into table 
+etable(
+  twfe_real_salary_no_exp_1, twfe_real_salary_no_exp_2, twfe_real_salary_no_exp_3, 
+  tex = TRUE,
+  tpt = TRUE,
+  dict = c(
+    real_salary = "Salary (Real) - No Experience Requirement",
+    treated_eff_full = "Treated $\\times$ Post",
+    state = "State",
+    date = "Month-Year",
+    soc_4 = "Occupation",
+    company = "Firm"
+  ),
+  style.tex = style.tex("aer"), # Removes weird "Model" thing and cleans up headings
+  notes = c(
+    "@\\footnotesize",
+    "*Notes:* Standard errors are clustered at the state level.",
+    "*** p$<$0.01, ** p$<$0.05, * p$<$0.1"
+  ),
+  file = "output/tables/table_twfe_real_salary_no_exp_salary_mn.tex"
+)
+
+# NOTE: Once I'm performing other methods too, I may need to figure out how to 
+# combine all of these results in a different table. This is fine for now.
+
+
+
+
+
+# --- 3. Event Study ---
+
+# (i) Spec 1: TWFE 
+# Run event study specification 
+es_real_salary_no_exp_1 <- feols(
+  real_salary ~ i(event_time, ban_full, -1) |
+    state + date,
+  data = es_df_no_exp,
+  cluster = ~state
+)
+
+summary(es_real_salary_no_exp_1)
+
+# create plotting dataframe 
+es_real_salary_no_exp_1_df <- tibble(
+  term = names(es_real_salary_no_exp_1$coefficients),
+  estimate = as.numeric(es_real_salary_no_exp_1$coefficients),
+  std_error = as.numeric(es_real_salary_no_exp_1$se)
+) %>%
+  mutate(
+    event_time = as.numeric(str_extract(term, "-?\\d+")),
+    ci_low = estimate - 1.96 * std_error, # 95% CIs 
+    ci_high = estimate + 1.96 * std_error
+  )
+
+ref_row <- tibble( #adding back in an entry for the omitted period 
+  term = "Reference",
+  estimate = 0, 
+  std_error = NA_real_,
+  event_time = -1,
+  ci_low = NA_real_,
+  ci_high = NA_real_
+)
+
+es_real_salary_no_exp_1_df <- bind_rows(es_real_salary_no_exp_1_df, ref_row) %>%
+  arrange(event_time)
+
+# Create plot with ggplot 
+es_plot_real_salary_no_exp_1 <- ggplot(es_real_salary_no_exp_1_df, aes(x = event_time, y = estimate)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
+  geom_vline(xintercept = -1, linetype = "dotted", color = "black") +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.4) + 
+  scale_x_continuous(breaks = seq(-36, 19, by = 3)) + 
+  labs(
+    x = "Months Since Ban",
+    y = "Coefficient - Salary (Real) - No Experience"
+  ) + 
+  theme_minimal() + 
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.ticks = element_line(color = "black")
+  )
+
+es_plot_real_salary_no_exp_1
+
+# Save the event study plot 
+ggsave(
+  "output/figures/es_real_salary_no_exp_1_salary_mn.pdf",
+  es_plot_real_salary_no_exp_1,
+  width = 7,
+  height = 4.5,
+  units = "in"
+)
+
+
+# (ii) Spec 2: Add Occupation FE
+# Run event study specification 
+es_real_salary_no_exp_2 <- feols(
+  real_salary ~ i(event_time, ban_full, -1) |
+    state + date + soc_4,
+  data = es_df_no_exp,
+  cluster = ~state
+)
+
+summary(es_real_salary_no_exp_2)
+
+# create plotting dataframe 
+es_real_salary_no_exp_2_df <- tibble(
+  term = names(es_real_salary_no_exp_2$coefficients),
+  estimate = as.numeric(es_real_salary_no_exp_2$coefficients),
+  std_error = as.numeric(es_real_salary_no_exp_2$se)
+) %>%
+  mutate(
+    event_time = as.numeric(str_extract(term, "-?\\d+")),
+    ci_low = estimate - 1.96 * std_error, # 95% CIs 
+    ci_high = estimate + 1.96 * std_error
+  )
+
+ref_row <- tibble( #adding back in an entry for the omitted period 
+  term = "Reference",
+  estimate = 0, 
+  std_error = NA_real_,
+  event_time = -1,
+  ci_low = NA_real_,
+  ci_high = NA_real_
+)
+
+es_real_salary_no_exp_2_df <- bind_rows(es_real_salary_no_exp_2_df, ref_row) %>%
+  arrange(event_time)
+
+# Create plot with ggplot 
+es_plot_real_salary_no_exp_2 <- ggplot(es_real_salary_no_exp_2_df, aes(x = event_time, y = estimate)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
+  geom_vline(xintercept = -1, linetype = "dotted", color = "black") +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.4) + 
+  scale_x_continuous(breaks = seq(-36, 19, by = 3)) + 
+  labs(
+    x = "Months Since Ban",
+    y = "Coefficient - Salary (Real) - No Experience"
+  ) + 
+  theme_minimal() + 
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.ticks = element_line(color = "black")
+  )
+
+es_plot_real_salary_no_exp_2
+
+# Save the event study plot 
+ggsave(
+  "output/figures/es_real_salary_no_exp_2_salary_mn.pdf",
+  es_plot_real_salary_no_exp_2,
+  width = 7,
+  height = 4.5,
+  units = "in"
+)
+
+
+# (iii) Spec 3: Add Firm FE 
+# Run event study specification 
+es_real_salary_no_exp_3 <- feols(
+  real_salary ~ i(event_time, ban_full, -1) |
+    state + date + soc_4 + company,
+  data = es_df_no_exp,
+  cluster = ~state
+)
+
+summary(es_real_salary_no_exp_3)
+
+# create plotting dataframe 
+es_real_salary_no_exp_3_df <- tibble(
+  term = names(es_real_salary_no_exp_3$coefficients),
+  estimate = as.numeric(es_real_salary_no_exp_3$coefficients),
+  std_error = as.numeric(es_real_salary_no_exp_3$se)
+) %>%
+  mutate(
+    event_time = as.numeric(str_extract(term, "-?\\d+")),
+    ci_low = estimate - 1.96 * std_error, # 95% CIs 
+    ci_high = estimate + 1.96 * std_error
+  )
+
+ref_row <- tibble( #adding back in an entry for the omitted period 
+  term = "Reference",
+  estimate = 0, 
+  std_error = NA_real_,
+  event_time = -1,
+  ci_low = NA_real_,
+  ci_high = NA_real_
+)
+
+es_real_salary_no_exp_3_df <- bind_rows(es_real_salary_no_exp_3_df, ref_row) %>%
+  arrange(event_time)
+
+# Create plot with ggplot 
+es_plot_real_salary_no_exp_3 <- ggplot(es_real_salary_no_exp_3_df, aes(x = event_time, y = estimate)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
+  geom_vline(xintercept = -1, linetype = "dotted", color = "black") +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.4) + 
+  scale_x_continuous(breaks = seq(-36, 19, by = 3)) + 
+  labs(
+    x = "Months Since Ban",
+    y = "Coefficient - Salary (Real) - No Experience"
+  ) + 
+  theme_minimal() + 
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.ticks = element_line(color = "black")
+  )
+
+es_plot_real_salary_no_exp_3
+
+# Save the event study plot 
+ggsave(
+  "output/figures/es_real_salary_no_exp_3_salary_mn.pdf",
+  es_plot_real_salary_no_exp_3,
+  width = 7,
+  height = 4.5,
+  units = "in"
+)
+
+
+
+
+
+
+
 
 
 
