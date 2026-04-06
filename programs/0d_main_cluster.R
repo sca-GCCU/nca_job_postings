64b4146afffc51f7c35d1949c8d9489af227a0ae
sca-GCCU <scanastasi18@gmail.com>
2026-04-06 17:52:33 -0400

    Change name of main cluster script

--- programs/0d_main_cluster.R
+++ programs/0d_main_cluster.R
@@ -9,7 +9,7 @@
 # Description: This script runs all of the analysis that takes place in the
 # Palmetto Cluster.
 #
-# Dependencies:
+# Dependencies: "0b_packages.R", "0c_paths.R"
 #
 # Output: 
 ##############################################################################
@@ -23,32 +23,33 @@ proj_root <- file.path(home, "nca_job_postings") # project root
 programs_dir    <- file.path(proj_root, "programs") # code directory
 source(file.path(programs_dir, "0c_paths.R")) # run paths helper 
 
+# 3. Install packages (COMMENT OUT AFTER FIRST RUN)
+source(file.path(programs_dir, "0b_packages.R")) 
 
 # NOTE: echo = TRUE can be used inside source to create a readable log of the 
 # code I ran and the outputs. 
 
-
-# 3. Prep Main Data
+# 4. Prep Main Data
 source(file.path(programs_dir, "3a_prep_covariates.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "3b_prep_agg1_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "3c_prep_agg2_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "3d_prep_salary_mn.R"), echo = TRUE, max.deparse.length = 1000)
 
-# 4. Prep Placebo Data 
+# 5. Prep Placebo Data 
 source(file.path(programs_dir, "3e_prep_agg1_placebo_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "3f_prep_agg2_placebo_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "3g_prep_salary_placebo_mn.R"), echo = TRUE, max.deparse.length = 1000)
 
-# 5. Summary Statistics 
+# 6. Summary Statistics 
 source(file.path(programs_dir, "4a_sumstats_agg1_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "4b_sumstats_agg2_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "4c_sumstats_salary_mn.R"), echo = TRUE, max.deparse.length = 1000)
 
-# 6. Descriptive Graphs 
+# 7. Descriptive Graphs 
 source(file.path(programs_dir, "5a_descriptive_graphs_agg1_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "5b_descriptive_graphs_salary_mn.R"), echo = TRUE, max.deparse.length = 1000)
 
-# 7. TWFE 
+# 8. TWFE 
 source(file.path(programs_dir, "6a_twfe_agg1_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "6b_twfe_agg2_mn.R"), echo = TRUE, max.deparse.length = 1000)
 source(file.path(programs_dir, "6c_twfe_salary_mn.R"), echo = TRUE, max.deparse.length = 1000)
