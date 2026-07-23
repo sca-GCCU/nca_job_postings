dd8a8058ef71f7ed77cec9015a9d2678f49b6d25
sca-GCCU <scanastasi18@gmail.com>
2026-07-23 16:31:39 -0400

    Create tables presenting overall treatment effect
    Created tables for CS DID analysis that summarize the overall treatment effect. This is for the firm-state-year level analysis.

--- programs/9a_cs_did_fab.R
+++ programs/9a_cs_did_fab.R
@@ -4,7 +4,7 @@
 #
 # R Script: "9a_cs_did_fab.R" 
 # by: Sebastian C. Anastasi
-# Date of this version: July 16, 2026
+# Date of this version: July 23, 2026
 #
 # Description: Runs CS DID analysis of states staggered noncompete bans for the
 # firm-soc-state-year level sample.
@@ -49,6 +49,7 @@ df_firm <- read_csv("data/analysis-data/agg2_fab_analysis.csv")
 # -------------------- CS DID ANALYSIS (UNCONDITIONAL) -------------------------
 # --- Total Postings --- 
 # NOTE: Unbalanced panel version (repeated cross-sections)
+
 cs_tot_post <- att_gt(
   yname = "total_postings_firm",
   tname = "year",
@@ -201,7 +202,44 @@ ggsave(
   units = "in"
 )
 
-rm(cs_any_exp, csdid_cohort_exp, es_any_exp, csdid_es_exp)
+# NOTE: Could later combine this little table with tables of other results.
+simple_any_exp <- aggte(cs_any_exp, type = "simple")
+
+tib_any_exp <- tibble(
+  `Estimate` = simple_any_exp$overall.att,
+  `Std. Error` = simple_any_exp$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_any_exp$overall.att - 1.96*simple_any_exp$overall.se,
+    simple_any_exp$overall.att + 1.96*simple_any_exp$overall.se
+  )
+)
+
+tab_any_exp <- kable(
+  tib_any_exp,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Experience Required (Postings)",
+  label = "csdid_tab_any_exp_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the number of postings requiring experience across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_any_exp,
+  "output/tables/csdid_tab_any_exp_fab.tex"
+)
+
+rm(cs_any_exp, csdid_cohort_exp, es_any_exp, csdid_es_exp,
+   tib_any_exp, tab_any_exp, simple_any_exp)
 gc()
 
 
@@ -261,7 +299,44 @@ ggsave(
   units = "in"
 )
 
-rm(cs_share_exp, csdid_cohort_exp_share, es_share_exp, csdid_es_exp_share)
+# NOTE: Could later combine this little table with tables of other results.
+simple_share_exp <- aggte(cs_share_exp, type = "simple")
+
+tib_share_exp <- tibble(
+  `Estimate` = simple_share_exp$overall.att,
+  `Std. Error` = simple_share_exp$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_share_exp$overall.att - 1.96*simple_share_exp$overall.se,
+    simple_share_exp$overall.att + 1.96*simple_share_exp$overall.se
+  )
+)
+
+tab_share_exp <- kable(
+  tib_share_exp,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Experience Required (Share)",
+  label = "csdid_tab_share_exp_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the share of postings requiring experience across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_share_exp,
+  "output/tables/csdid_tab_share_exp_fab.tex"
+)
+
+rm(cs_share_exp, csdid_cohort_exp_share, es_share_exp, csdid_es_exp_share,
+   tib_share_exp, tab_share_exp, simple_share_exp)
 gc()
 
 
@@ -321,7 +396,44 @@ ggsave(
   units = "in"
 )
 
-rm(cs_ave_exp, csdid_cohort_exp_ave, es_ave_exp, csdid_es_exp_ave)
+# NOTE: Could later combine this little table with tables of other results.
+simple_ave_exp <- aggte(cs_ave_exp, type = "simple")
+
+tib_ave_exp <- tibble(
+  `Estimate` = simple_ave_exp$overall.att,
+  `Std. Error` = simple_ave_exp$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_ave_exp$overall.att - 1.96*simple_ave_exp$overall.se,
+    simple_ave_exp$overall.att + 1.96*simple_ave_exp$overall.se
+  )
+)
+
+tab_ave_exp <- kable(
+  tib_ave_exp,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Experience Required (Average)",
+  label = "csdid_tab_ave_exp_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on average required experience across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_ave_exp,
+  "output/tables/csdid_tab_ave_exp_fab.tex"
+)
+
+rm(cs_ave_exp, csdid_cohort_exp_ave, es_ave_exp, csdid_es_exp_ave,
+   tib_ave_exp, tab_ave_exp, simple_ave_exp)
 gc()
 
 
@@ -381,7 +493,44 @@ ggsave(
   units = "in"
 )
 
-rm(cs_bachelor, csdid_cohort_bachelor, es_bachelor, csdid_es_bachelor)
+# NOTE: Could later combine this little table with tables of other results.
+simple_bachelor <- aggte(cs_bachelor, type = "simple")
+
+tib_bachelor <- tibble(
+  `Estimate` = simple_bachelor$overall.att,
+  `Std. Error` = simple_bachelor$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_bachelor$overall.att - 1.96*simple_bachelor$overall.se,
+    simple_bachelor$overall.att + 1.96*simple_bachelor$overall.se
+  )
+)
+
+tab_bachelor <- kable(
+  tib_bachelor,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Bachelor's Required (Postings)",
+  label = "csdid_tab_bachelor_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the number of postings requiring a bachelor's degree across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_bachelor,
+  "output/tables/csdid_tab_bachelor_fab.tex"
+)
+
+rm(cs_bachelor, csdid_cohort_bachelor, es_bachelor, csdid_es_bachelor,
+   tib_bachelor, tab_bachelor, simple_bachelor)
 gc()
 
 
@@ -441,10 +590,49 @@ ggsave(
   units = "in"
 )
 
+# NOTE: Could later combine this little table with tables of other results.
+simple_bachelor_share <- aggte(cs_bachelor_share, type = "simple")
+
+tib_bachelor_share <- tibble(
+  `Estimate` = simple_bachelor_share$overall.att,
+  `Std. Error` = simple_bachelor_share$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_bachelor_share$overall.att - 1.96*simple_bachelor_share$overall.se,
+    simple_bachelor_share$overall.att + 1.96*simple_bachelor_share$overall.se
+  )
+)
+
+tab_bachelor_share <- kable(
+  tib_bachelor_share,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Bachelor's Required (Share)",
+  label = "csdid_tab_bachelor_share_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the share of postings requiring a bachelor's degree across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_bachelor_share,
+  "output/tables/csdid_tab_bachelor_share_fab.tex"
+)
+
 rm(cs_bachelor_share, 
    csdid_cohort_bachelor_share, 
    es_bachelor_share, 
-   csdid_es_bachelor_share)
+   csdid_es_bachelor_share,
+   tib_bachelor_share,
+   tab_bachelor_share,
+   simple_bachelor_share)
 gc()
 
 
@@ -504,7 +692,44 @@ ggsave(
   units = "in"
 )
 
-rm(cs_fulltime, csdid_cohort_fulltime, es_fulltime, csdid_es_fulltime)
+# NOTE: Could later combine this little table with tables of other results.
+simple_fulltime <- aggte(cs_fulltime, type = "simple")
+
+tib_fulltime <- tibble(
+  `Estimate` = simple_fulltime$overall.att,
+  `Std. Error` = simple_fulltime$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_fulltime$overall.att - 1.96*simple_fulltime$overall.se,
+    simple_fulltime$overall.att + 1.96*simple_fulltime$overall.se
+  )
+)
+
+tab_fulltime <- kable(
+  tib_fulltime,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Full-Time Postings",
+  label = "csdid_tab_fulltime_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the number of full-time postings across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_fulltime,
+  "output/tables/csdid_tab_fulltime_fab.tex"
+)
+
+rm(cs_fulltime, csdid_cohort_fulltime, es_fulltime, csdid_es_fulltime,
+   tib_fulltime, tab_fulltime, simple_fulltime)
 gc()
 
 
@@ -564,10 +789,49 @@ ggsave(
   units = "in"
 )
 
+# NOTE: Could later combine this little table with tables of other results.
+simple_fulltime_share <- aggte(cs_fulltime_share, type = "simple")
+
+tib_fulltime_share <- tibble(
+  `Estimate` = simple_fulltime_share$overall.att,
+  `Std. Error` = simple_fulltime_share$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_fulltime_share$overall.att - 1.96*simple_fulltime_share$overall.se,
+    simple_fulltime_share$overall.att + 1.96*simple_fulltime_share$overall.se
+  )
+)
+
+tab_fulltime_share <- kable(
+  tib_fulltime_share,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Full-Time Postings (Share)",
+  label = "csdid_tab_fulltime_share_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the share of postings that are full-time across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_fulltime_share,
+  "output/tables/csdid_tab_fulltime_share_fab.tex"
+)
+
 rm(cs_fulltime_share, 
    csdid_cohort_fulltime_share, 
    es_fulltime_share, 
-   csdid_es_fulltime_share)
+   csdid_es_fulltime_share,
+   tib_fulltime_share,
+   tab_fulltime_share,
+   simple_fulltime_share)
 gc()
 
 
@@ -627,7 +891,44 @@ ggsave(
   units = "in"
 )
 
-rm(cs_parttime, csdid_cohort_parttime, es_parttime, csdid_es_parttime)
+# NOTE: Could later combine this little table with tables of other results.
+simple_parttime <- aggte(cs_parttime, type = "simple")
+
+tib_parttime <- tibble(
+  `Estimate` = simple_parttime$overall.att,
+  `Std. Error` = simple_parttime$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_parttime$overall.att - 1.96*simple_parttime$overall.se,
+    simple_parttime$overall.att + 1.96*simple_parttime$overall.se
+  )
+)
+
+tab_parttime <- kable(
+  tib_parttime,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Part-Time Postings",
+  label = "csdid_tab_parttime_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the number of part-time postings across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_parttime,
+  "output/tables/csdid_tab_parttime_fab.tex"
+)
+
+rm(cs_parttime, csdid_cohort_parttime, es_parttime, csdid_es_parttime,
+   tib_parttime, tab_parttime, simple_parttime)
 gc()
 
 
@@ -687,10 +988,49 @@ ggsave(
   units = "in"
 )
 
+# NOTE: Could later combine this little table with tables of other results.
+simple_parttime_share <- aggte(cs_parttime_share, type = "simple")
+
+tib_parttime_share <- tibble(
+  `Estimate` = simple_parttime_share$overall.att,
+  `Std. Error` = simple_parttime_share$overall.se,
+  `95% CI` = sprintf(
+    "[%.3f, %.3f]",
+    simple_parttime_share$overall.att - 1.96*simple_parttime_share$overall.se,
+    simple_parttime_share$overall.att + 1.96*simple_parttime_share$overall.se
+  )
+)
+
+tab_parttime_share <- kable(
+  tib_parttime_share,
+  format = "latex",
+  booktabs = TRUE,
+  digits = 3,
+  align = c("ccc"),
+  caption = "Overall Effect - Part-Time Postings (Share)",
+  label = "csdid_tab_parttime_share_fab"
+) %>%
+  kable_styling(latex_options = c("hold_position")) %>%
+  footnote(
+    general_title = "",
+    fixed_small_size = TRUE,
+    general = "\\\\footnotesize \\\\textit{Notes:} This table reports the overall effect of NCA bans on the share of postings that are part-time across all treated states.",
+    threeparttable = TRUE,
+    escape = FALSE
+  ) 
+
+writeLines(
+  tab_parttime_share,
+  "output/tables/csdid_tab_parttime_share_fab.tex"
+)
+
 rm(cs_parttime_share, 
    csdid_cohort_parttime_share,
    es_parttime_share,
-   csdid_es_parttime_share)
+   csdid_es_parttime_share,
+   tib_parttime_share,
+   tab_parttime_share,
+   simple_parttime_share)
 gc()
 
 
